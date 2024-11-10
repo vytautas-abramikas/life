7a9856c300266a35647c02896a891c6c1880e33e
broliux <abramikas@gmail.com>
2024-11-11 01:43:36 +0200

    editing functionality

--- src/components/Lattice.tsx
+++ src/components/Lattice.tsx
@@ -2,14 +2,30 @@ import { useAppContext } from "../hooks/useAppContext";
 import { getCellColor } from "../lib/getCellColor";
 
 export const Lattice: React.FC = () => {
-  const { latticeWidth, latticeHeight, latticeType, currentLattice } =
-    useAppContext();
+  const {
+    latticeWidth,
+    latticeHeight,
+    latticeType,
+    currentLattice,
+    setCurrentLattice,
+  } = useAppContext();
 
   if (!currentLattice) return null;
 
   const cellSizeWidth = window.innerWidth / latticeWidth;
   const cellSizeHeight = (window.innerHeight * 0.9) / latticeHeight;
   const cellSizeRem = Math.min(cellSizeWidth, cellSizeHeight) / 17;
+
+  const toggleCell = (y: number, x: number) => {
+    setCurrentLattice((prev) => {
+      if (prev) {
+        const newGrid = prev.map((row) => [...row]);
+        newGrid[y][x] = !newGrid[y][x];
+        return newGrid;
+      }
+      return prev;
+    });
+  };
 
   return (
     <div className="flex justify-center items-center h-[90vh]">
@@ -30,6 +46,7 @@ export const Lattice: React.FC = () => {
                 currentLattice,
                 latticeType
               )}`}
+              onClick={() => toggleCell(yIndex, xIndex)}
             ></div>
           ))
         )}

f60792dba4f0ec5d847f42bd705a4db26f01409f
broliux <abramikas@gmail.com>
2024-11-13 01:19:56 +0200

    playing with styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,16 @@ import { Controls } from "./Controls";
 
 export const Header: React.FC = () => {
   return (
-    <div className="w-full min-h-[10vh] bg-black grid grid-cols-2">
-      <Counter />
-      <Controls />
+    <div
+      className="w-full min-h-[10vh] bg-black grid grid-cols-5"
+      style={{ fontSize: "min(1.8vh, 1.8vw)" }}
+    >
+      <div className="col-start-2 col-span-1">
+        <Counter />
+      </div>
+      <div className="col-span-2">
+        <Controls />
+      </div>
     </div>
   );
 };

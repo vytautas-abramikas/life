976e8c051080eaf0cc99b46fddf841d17f18cc8f
broliux <abramikas@gmail.com>
2024-11-12 23:01:12 +0200

    Updates

---
+++ assets/lwss-DG8iTLik.js
@@ -0,0 +1,1 @@
+const h=5,i=4,t=[[1,0,0,1,0],[0,0,0,0,1],[1,0,0,0,1],[0,1,1,1,1]],e={width:5,height:4,lattice:t};export{e as default,i as height,t as lattice,h as width};
---
+++ assets/mwss-CrVTeysH.js
@@ -0,0 +1,1 @@
+const h=6,i=5,t=[[0,0,1,0,0,0],[1,0,0,0,1,0],[0,0,0,0,0,1],[1,0,0,0,0,1],[0,1,1,1,1,1]],e={width:6,height:5,lattice:t};export{e as default,i as height,t as lattice,h as width};

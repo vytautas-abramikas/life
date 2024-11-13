0811d53de344553d948d99feefa88dab81f848ae
broliux <abramikas@gmail.com>
2024-11-13 03:20:38 +0200

    duh styling

--- src/components/FullScreenButton.tsx
+++ src/components/FullScreenButton.tsx
@@ -20,7 +20,12 @@ const toggleFullscreen = () => {
 export const FullScreenButton: React.FC = React.memo(() => {
   return (
     <button
-      className="fixed top-0 left-0 mt-4 ml-6 flex items-center justify-center hover:scale-110 hover:opacity-50 transition duration-500 ease-out bg-black opacity-25 rounded-xl text-4xl text-white h-16 w-16"
+      className="fixed top-0 left-0 flex items-center justify-center hover:scale-110 hover:opacity-50 transition duration-500 ease-out bg-black opacity-25 rounded-xl text-4xl text-white h-16 w-16"
+      style={{
+        fontSize: "min(4vh, 4vw)",
+        marginTop: "0.2em",
+        marginLeft: "0.2em",
+      }}
       onClick={toggleFullscreen}
     >
       🔳
